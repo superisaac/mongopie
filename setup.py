c0ca91f00a3cc696855f6f482ba158fa190bada1
Zeng Ke <superisaac.ke@gmail.com>
2011-12-18 00:32:22 +0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import distutils.core
+import sys
+
+distutils.core.setup(
+    name='mongopie',
+    version='0.1.0',
+    py_modules=['mongopie'],
+    author='superisaac',
+    author_email='superisaac.ke@gmail.com',
+    url='https://github.com/superisaac/mongopie',
+    license='http://www.opensource.org/licenses/mit-license.php',
+    description='mongopie is a mongodb-object mapping in python'
+)
